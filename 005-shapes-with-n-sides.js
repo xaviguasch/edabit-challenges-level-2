70f96d11d257cea391a91200c8a33819cf399c1b
xaviguasch <xaviguasch@gmail.com>
2020-06-14 19:35:31 +0200

    added solution to n shape

---
+++ 005-shapes-with-n-sides.js
@@ -0,0 +1,11 @@
+function nSidedShape(n) {
+  switch (n) {
+    case 1:
+      return 'circle'
+      break
+  }
+}
+
+console.log(nSidedShape(3))
+console.log(nSidedShape(1))
+console.log(nSidedShape(9))
