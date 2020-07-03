3fd3797cbf5854ec22e690d7d4bc0ce0b8191958
xaviguasch <xaviguasch@gmail.com>
2020-07-03 22:48:03 +0200

    added problem Special Arrays

---
+++ 042-special-arrays.js
@@ -0,0 +1,5 @@
+function isSpecialArray(arr) {}
+
+// An array is special, if every even index contains an even number and every odd index contains an odd number. Create a function that returns true if an array is special, and false otherwise.
+
+console.log(isSpecialArray([2, 7, 4, 9, 6, 1, 6, 3]))
