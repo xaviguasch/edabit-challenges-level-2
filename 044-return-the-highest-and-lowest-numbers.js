377bca1fffaffb2b6ca411edac3c151614cb08cb
xaviguasch <xaviguasch@gmail.com>
2020-07-06 21:50:09 +0200

    added problem: Return the Highest and Lowest Numbers

---
+++ 044-return-the-highest-and-lowest-numbers.js
@@ -0,0 +1,10 @@
+function highLow(str) {}
+
+console.log(highLow('1 2 -3 4 5'))
+console.log(highLow('13'))
+
+// Create a function that accepts a string of space separated numbers and returns the highest and lowest number (as a string).
+
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
