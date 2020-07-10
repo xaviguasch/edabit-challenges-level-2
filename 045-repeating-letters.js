7988c3e02972127d9035846c3591e07d69135239
xaviguasch <xaviguasch@gmail.com>
2020-07-10 20:54:17 +0200

    added solution for: Repeated Letters

--- 045-repeating-letters.js
+++ 045-repeating-letters.js
@@ -1,4 +1,8 @@
-function doubleChar(str) {}
+function doubleChar(str) {
+  const splitted = str.split('')
+
+  return splitted.map((letter) => letter + letter).join('')
+}
 
 // Create a function that takes a string and returns a string in which each character is repeated once.
 
