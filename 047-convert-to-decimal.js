0634b49c111ca79bac5ab212dce138199251babf
xaviguasch <xaviguasch@gmail.com>
2020-11-11 21:34:54 +0100

    added solution for CONVERT TO DECIMAL NOTATION

--- 047-convert-to-decimal.js
+++ 047-convert-to-decimal.js
@@ -1,5 +1,7 @@
 // Create a function to convert an array of percentages to their decimal equivalents.
 
-function convertToDecimal() {}
+function convertToDecimal(arr) {
+  return arr.map((perc) => parseFloat(perc) / 100)
+}
 
 console.log(convertToDecimal(['33%', '98.1%', '56.44%', '100%']))
