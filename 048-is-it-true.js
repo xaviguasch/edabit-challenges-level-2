3b30a6e1dffd021d7dac778d92a0cdcd7aece67e
xaviguasch <xaviguasch@gmail.com>
2020-11-23 18:48:22 +0100

    added solution to IS IT TRUE exercise

--- 048-is-it-true.js
+++ 048-is-it-true.js
@@ -1,4 +1,6 @@
-function isTrue(relation) {}
+function isTrue(relation) {
+  return eval(relation.replace('=', '==='))
+}
 
 // In this challenge you will be given a relation between two numbers, written as a string. Write a function that determines if the relation is true or false.
 
